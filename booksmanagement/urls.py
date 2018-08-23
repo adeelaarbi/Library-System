b065dcf4fc6754b4fb9bf75e426c1d28ddd7d2ce
adeel <adeel.aarbi92@gmail.com>
2018-08-23 13:14:10 +0500

    .

--- booksmanagement/urls.py
+++ booksmanagement/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('rack/', include("racks.urls"))
+    path('', include("racks.urls")),
+    path('admin/', admin.site.urls)
 ]
+
--- racks/urls.py
+++ racks/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from racks import views
 
 urlpatterns = [
-    path('', views.BookListView.as_view(), name="home"),
+    path('book/list/', views.BookListView.as_view(), name="book-list"),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.logout, {'next_page': 'login'}, name='logout'),
     path('login/', auth_views.login, {'template_name': 'login.html'}, name='login'),
